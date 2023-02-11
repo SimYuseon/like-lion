c667677efb114a166731a06a8e87e9944abcf373
simyuseon <yseon0508@naver.com>
2023-02-11 17:00:38 +0900

    [학습] 미들웨어 함수생성 및 모듈화 적용 확인

--- code-lion/first-project/routes/index.js
+++ code-lion/first-project/routes/index.js
@@ -1,44 +1,52 @@
 var express = require("express");
 var router = express.Router();
-let arr = [];
+const loginCheck = require("../module/loginCheck");
 
-// Get method
-router.get("/read", (req, res) => {
-  res.status(200).json({
-    message: "read success",
-  });
-});
+// let arr = [];
 
-// Post method
-router.post("/create", (req, res) => {
-  // const data = req.body.data;
-  const { data } = req.body;
-  arr.push(data);
-  res.status(200).json({
-    message: "create success",
-    result: arr,
-  });
-});
+// // Get method
+// router.get("/read", (req, res) => {
+//   res.status(200).json({
+//     message: "read success",
+//   });
+// });
 
-// Put method
-router.put("/update/:id", (req, res) => {
-  const { id } = req.params;
-  const { data } = req.body;
-  arr[id] = data;
-  res.status(200).json({
-    message: "update success",
-    result: arr,
-  });
-  console.log(id);
-});
+// // Post method
+// router.post("/create", (req, res) => {
+//   // const data = req.body.data;
+//   const { data } = req.body;
+//   arr.push(data);
+//   res.status(200).json({
+//     message: "create success",
+//     result: arr,
+//   });
+// });
 
-// Delete method
-router.delete("/delete/:id", (req, res) => {
-  const { id } = req.params;
-  arr.splice(id, 1);
+// // Put method
+// router.put("/update/:id", (req, res) => {
+//   const { id } = req.params;
+//   const { data } = req.body;
+//   arr[id] = data;
+//   res.status(200).json({
+//     message: "update success",
+//     result: arr,
+//   });
+//   console.log(id);
+// });
+
+// // Delete method
+// router.delete("/delete/:id", (req, res) => {
+//   const { id } = req.params;
+//   arr.splice(id, 1);
+//   res.status(200).json({
+//     message: "delete success",
+//     result: arr,
+//   });
+// });
+
+router.get("/", loginCheck, (req, res) => {
   res.status(200).json({
-    message: "delete success",
-    result: arr,
+    message: "login success",
   });
 });
 
