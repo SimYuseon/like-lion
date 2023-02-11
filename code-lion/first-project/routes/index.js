1d743c35d165538b90a96ce2020b0310aac69092
simyuseon <yseon0508@naver.com>
2023-02-11 16:49:45 +0900

    [학습] script nodemon으로 수정 및 httpmethod postman으로 학습

--- code-lion/first-project/routes/index.js
+++ code-lion/first-project/routes/index.js
@@ -1,15 +1,45 @@
 var express = require("express");
 var router = express.Router();
+let arr = [];
 
-/* GET home page. */
+// Get method
+router.get("/read", (req, res) => {
+  res.status(200).json({
+    message: "read success",
+  });
+});
 
-router.post("/main", (req, res) => {
-  const data = req.body.data;
-  // res.send("문자열 응답"); //문자열 형식
-  // res.json({
-  //   message: "json응답", //json 형식
-  // });
-  // res.render("index"); //html 코드를 보여줌
+// Post method
+router.post("/create", (req, res) => {
+  // const data = req.body.data;
+  const { data } = req.body;
+  arr.push(data);
+  res.status(200).json({
+    message: "create success",
+    result: arr,
+  });
+});
+
+// Put method
+router.put("/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { data } = req.body;
+  arr[id] = data;
+  res.status(200).json({
+    message: "update success",
+    result: arr,
+  });
+  console.log(id);
+});
+
+// Delete method
+router.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  arr.splice(id, 1);
+  res.status(200).json({
+    message: "delete success",
+    result: arr,
+  });
 });
 
 module.exports = router;
